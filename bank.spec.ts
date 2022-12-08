a58dd30440a6173789eb31802f1564920ef093fd
Braunschweig Malo <malo.braunschweig@live.fr>
2022-12-08 16:20:34 +0100

    adding deposit func  and tests

--- bank.spec.ts
+++ bank.spec.ts
@@ -1,4 +1,19 @@
 import { Bank } from './bank'
 
 
-
+describe('Deposit', () => {
+    it.each([
+      [10, 20, 30],
+      [20, 20, 40],
+    ])('should deposit %i into %i and return %i', (amount, balance, expected) => {
+      expect(new Bank(balance).deposit(amount)).toEqual(expected)
+    })
+  
+    it.each([
+      [-10, 20],
+      [-20, 20],
+      [-30, 20],
+    ])('should throw an error when depositing %i into %i', (amount, balance) => {
+      expect(() => new Bank(balance).deposit(amount)).toThrowError("Can't add a negative amount")
+    })
+  })
--- bank.ts
+++ bank.ts
@@ -1,3 +1,11 @@
 export class Bank {
 	constructor(private balance: number) {}
+
+	deposit(amount: number) {
+        if (amount > 0) {
+            return (this.balance += amount)
+        } else {
+            throw new Error("Can't add a negative amount")
+        }
+    }
 }
