f2d7b6b0c9203d8989e58a5114157b64abe01189
Julien COUSIN-ALLIOT <julien.cousinalliot@gmail.com>
2022-12-08 16:16:14 +0100

    feat(withdraw): add withdraw

--- bank.spec.ts
+++ bank.spec.ts
@@ -1,4 +1,19 @@
 import { Bank } from './bank'
 
+describe('Withdraw', () => {
+    it.each([
+        [10, 20, 10],
+        [20, 20, 0],
+    ])('should withdraw %i from %i and return %i', (amount, balance, expected) => {
+        expect(new Bank(balance).withdraw(amount)).toEqual(expected)
+    })
 
+    it.each([
+        [30, 20],
+        [40, 20],
+        [50, 20],
+    ])('should throw an error when withdrawing %i from %i', (amount, balance) => {
+        expect(() => new Bank(balance).withdraw(amount)).toThrowError('Insufficient funds')
+    })
+})
 
--- bank.ts
+++ bank.ts
@@ -1,3 +1,10 @@
 export class Bank {
 	constructor(private balance: number) {}
+
+	withdraw(amount: number) {
+		if (amount > this.balance) {
+			throw new Error('Insufficient funds')
+		}
+		return (this.balance -= amount)
+	}
 }
