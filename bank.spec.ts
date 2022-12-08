25a5d9807f30edb30922916079653dc944a7eb38
Quintin Florent <55179969+Boloblux@users.noreply.github.com>
2022-12-08 16:22:34 +0100

    :money_with_wings: Currency Converter

--- bank.spec.ts
+++ bank.spec.ts
@@ -1,4 +1,18 @@
 import { Bank } from './bank'
 
+describe('Currency Converter', () => {
+    it.each([
+        [10, '€', 10],
+        [10, '$', 10.5],
+        [10, '£', 8.6],
+    ])('should convert %i %s to %s and return %i', (amount, currencyOut, expected) => {
+        expect(new Bank(0).currencyConverter(amount, currencyOut)).toEqual(expected)
+    })
 
-
+    it.each([
+        [10, '¥'],
+        [20, 'kr'],
+    ])('should throw an error when converting %i %s', (amount, currencyOut) => {
+        expect(() => new Bank(0).currencyConverter(amount, currencyOut)).toThrowError('Currency not found')
+    })
+})
--- bank.ts
+++ bank.ts
@@ -1,3 +1,21 @@
+const currencies = [
+	{ value: 1, symbol: '€' },
+	{ value: 1.05, symbol: '$' },
+	{ value: 0.86, symbol: '£' },
+]
+
 export class Bank {
-	constructor(private balance: number) {}
+	constructor(private balance: number) { }
+
+	currencyConverter(amount: number, currencyOut: string) {
+		if (currencyOut === currencies[0].symbol) {
+			return amount
+		} else {
+			const currency = currencies.find((currency) => currency.symbol === currencyOut)
+			if (!currency) {
+				throw new Error('Currency not found')
+			}
+			return amount * currency.value
+		}
+	}
 }
