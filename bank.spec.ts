f2e0f854db48d4161732ea8a61954ee5f4e746bc
Braunschweig Malo <70762378+AluSinVIII@users.noreply.github.com>
2022-12-08 16:46:26 +0100

    :sparkles: FEAT deposit
    Co-authored-by: Nicolas de Garrigues <70767445+Lainocs@users.noreply.github.com>

--- bank.spec.ts
+++ bank.spec.ts
@@ -7,7 +7,7 @@ describe('Withdraw', () => {
     ])('should withdraw %i from %i and return %i', (amount, balance, expected) => {
         expect(new Bank(balance).withdraw(amount)).toEqual(expected)
     })
-
+    
     it.each([
         [30, 20],
         [40, 20],
@@ -17,6 +17,22 @@ describe('Withdraw', () => {
     })
 })
 
+describe('Deposit', () => {
+    it.each([
+      [10, 20, 30],
+      [20, 20, 40],
+    ])('should deposit %i into %i and return %i', (amount, balance, expected) => {
+      expect(new Bank(balance).deposit(amount)).toEqual(expected)
+    })
+  
+    it.each([
+      [-10, 20],
+      [-20, 20],
+      [-30, 20],
+    ])('should throw an error when depositing %i into %i', (amount, balance) => {
+      expect(() => new Bank(balance).deposit(amount)).toThrowError("Can't add a negative amount")
+    })
+  })
 
 describe('Get Balance', () => {
     it.each([
--- bank.ts
+++ bank.ts
@@ -1,16 +1,24 @@
 export class Bank {
 	constructor(private balance: number) {}
 
-	withdraw(amount: number) {
+	account: string = 'Account Opened';
+
+	deposit(amount: number) {
+        if (amount > 0) {
+            return (this.balance += amount)
+        } else {
+            throw new Error("Can't add a negative amount")
+        }
+  }
+	
+  withdraw(amount: number) {
 		if (amount > this.balance) {
 			throw new Error('Insufficient funds')
 		}
 		return (this.balance -= amount)
 	}
 
-	account: string = 'Account Opened';
-
 	getBalance() {
-        return this.balance
-    }
+      return this.balance
+  }
 }
